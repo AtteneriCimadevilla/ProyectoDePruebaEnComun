9db2e15010e09be835a7d0913969606b1c35f084
Atteneri Maria Rodriguez Sanchez <attrodsan@alu.edu.gva.es>
2024-01-19 03:04:40 +0100

    Commit ramanueva 2

--- src/proyectodepruebaencomun/ProyectoDePruebaEnComun.java
+++ src/proyectodepruebaencomun/ProyectoDePruebaEnComun.java
@@ -14,13 +14,15 @@ public class ProyectoDePruebaEnComun {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
 
         System.out.println("Hello, Denise! perdon por borrarte mady!!!");
-        //You can delete this line and send it back.
+        
 
         System.out.println("Hola de nuevo, Mady! Denise me borró tu línea...");
-        //You can delete this line and send it back.
+        
+        
+        System.out.println("Hello, Denise! perdon por borrarte mady!!!");
 
     }
 
